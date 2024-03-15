31f6baf240747e1a5a25d178dfe1009fb2e70662
love-ara <151339306+love-ara@users.noreply.github.com>
2024-03-15 04:51:38 +0100

    Update MyLoveLetter.java

--- MyLoveLetter.java
+++ MyLoveLetter.java
@@ -1,29 +1,31 @@
       public class MyLoveLetter{
 
-      public static void main (String [] args){
+            public static void main(String[] args) {
 
 
-        System.out.printf("									Semicolon Africa,%n									320, Herbert Macaulay,%n								        Sabo, Yaba,%n									Lagos, Nigeria.%n");												 																											System.out.printf("25, Thomas street,%nOff Adeniji-Adele,%nLagos.%n%n ");
-      
+        System.out.print("""
+                Semicolon Africa
+                320, Herbert Macaulay,
+                Sabo, Yaba,
+                Lagos, Nigeria.
+               
+                                                                   25, Thomas street,
+                                                                   Off Adeniji-Adele,
+                                                                   Lagos
+                                                                   """);
+        System.out.print("""
+                Dear Reader,\s
+                   I hope this letter meets you well. How are you and how is life treating you?.
+                I am writing to cheer you up and on, that whatever you are doing, you should keep going because
+                you are doing amazing stuff for yourself and I am rooting for, now and always. I love you,
+                keep being you. Till i write again, be good and keep doing amazing.
+                                                           
+                                                                                your cheerleader,
+                                                                                            Ara.
+                                                                                               """);
 
-      System.out.printf(" Dear Reader,%n%n");
-       
-      System.out.printf("   I hope this letter meets you well. How are you and how is life treating you?.%nI am writing to cheer you up and on, that whatever you are doing,%nyou should keep going because you are doing amazing stuff for yourself and%n I am rooting for, now and always.%n%n   I love you, keep being you. Till i write again, be good and keep doing amazing.%n%n"  );
 
-     
-      System.out.printf("  your cheerleader,%n  Ara.");
-
-     
-
-
-
-
-
-
-
-
-
-
+    }
 }
 
 
@@ -40,6 +42,3 @@
 
 
 
-
-
-}
